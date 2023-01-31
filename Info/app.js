be9b108f2dcd4fa6bdd1fa5a33bc751a2132a830
TuvaStange <tuvsta13@o365.mrvgs.no>
2023-01-31 11:15:00 +0100

    Proffe front-end folk ass

---
+++ Info/app.js
@@ -0,0 +1,14 @@
+import { VudIconModule } from '@vismaux/ngx-vud';
+
+let infobtn = document.getElementById('info');
+let feedbackbtn = document.getElementById("feedback");
+
+function iselectedbtn() {
+    infobtn.style.backgroundColor = "#ffffff";
+    feedbackbtn.style.backgroundColor = "#ECEEF1";
+}
+
+function fselectedbtn() {
+    feedbackbtn.style.backgroundColor = "#ffffff";
+    infobtn.style.backgroundColor = "#ECEEF1";
+}
