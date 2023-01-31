41711cf33514731400947e56f8be1de9548a7511
Tangen <Tangen>
2023-01-31 12:41:56 +0100

    mhm

--- Info/app.js
+++ Info/app.js
@@ -1,14 +1,22 @@
-import { VudIconModule } from '@vismaux/ngx-vud';
-
 let infobtn = document.getElementById('info');
 let feedbackbtn = document.getElementById("feedback");
+let infobtn1 = document.getElementById('info1');
+let feedbackbtn1 = document.getElementById("feedback1");
 
 function iselectedbtn() {
-    infobtn.style.backgroundColor = "#ffffff";
-    feedbackbtn.style.backgroundColor = "#ECEEF1";
+    infobtn.style.backgroundColor = "#ECEEF1";
+    feedbackbtn.style.backgroundColor = "#ffffff";
+    infobtn1.style.backgroundColor = "#ECEEF1";
+    feedbackbtn1.style.backgroundColor = "#ffffff";
+    document.getElementById("feed-container").style.display = "none";
+    document.getElementById("info-hide").style.display = "block";
 }
 
 function fselectedbtn() {
-    feedbackbtn.style.backgroundColor = "#ffffff";
-    infobtn.style.backgroundColor = "#ECEEF1";
+    feedbackbtn.style.backgroundColor = "#ECEEF1";
+    infobtn.style.backgroundColor = "#ffffff";
+    feedbackbtn1.style.backgroundColor = "#ECEEF1";
+    infobtn1.style.backgroundColor = "#ffffff";
+    document.getElementById("feed-container").style.display = "block";
+    document.getElementById("info-hide").style.display = "none";
 }
