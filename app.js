6440f0278e5d33b2f10100c61d22ae2f18c63c80
Aslak <99125865+Tabgen@users.noreply.github.com>
2023-01-31 10:11:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let infobtn = document.getElementById('info');
+let feedbackbtn = document.getElementById("feedback");
+
+function iselectedbtn() {
+    infobtn.style.backgroundColor = "#ffffff";
+    feedbackbtn.style.backgroundColor = "#ECEEF1";
+}
+
+function fselectedbtn() {
+    feedbackbtn.style.backgroundColor = "#ffffff";
+    infobtn.style.backgroundColor = "#ECEEF1";
+}
